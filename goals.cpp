b8a9894cb67677daee2cfb90ad1192a6644296c5
Sagnik Mukhopadhaya <sagnikcodes.in@gmail.com>
2026-07-20 23:49:11 +0530

    Added 1877A problem

---
+++ goals.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>a(n);
+        for(int i=0; i<n-1; i++)
+        {
+            cin>>a[i];
+        }
+        int sum=0;
+        for(int i=0; i<n-1; i++)
+        {
+            sum=sum+a[i];
+        }
+        cout<<-sum<<endl;
+    }
+}
