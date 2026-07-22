50ba817986c03a77ebb00c8951d4242904a3b7d8
Sagnik Mukhopadhaya <sagnikcodes.in@gmail.com>
2026-07-22 20:31:00 +0530

    Added problem 1858A

---
+++ buttons.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a>b) cout<<"First"<<endl;
+        else if(a<b) 
+            cout<<"Second"<<endl;
+        else
+        {
+            if(c%2==0)
+                cout<<"Second"<<endl;
+            else  
+                cout<<"First"<<endl;
+        }
+    }
+    return 0;
+}
