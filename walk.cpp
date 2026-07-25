4667a848001229c75fdae23fe702976fbe64f1ec
Sagnik Mukhopadhaya <sagnikcodes.in@gmail.com>
2026-07-25 12:56:54 +0530

    Added problem 1806A

---
+++ walk.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long a, b, c, d;
+        cin >> a >> b >> c >>d;
+        if(b<=d && c<=a+(d-b))
+        {
+            cout<<(d-b) + (a+d-b-c)<<endl;
+        }
+        else
+            cout<<-1<<endl;
+    }
+    return 0;
+}
