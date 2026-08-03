7a185bea7878d71f878103d41b564fd53d835396
Sagnik Mukhopadhaya <sagnikcodes.in@gmail.com>
2026-08-03 22:05:59 +0530

    Added problem 1794B

---
+++ ndiv.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        if (a[0] == 1)
+        {
+            a[0]++;
+        }
+        for (int i = 1; i < n; i++)
+        {
+            if (a[i] == 1)
+            {
+                a[i]++;
+            }
+            while (a[i] % a[i - 1] == 0)
+            {
+                a[i]++;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
