2b0d87a5dd813fcd5bb1ffe14715e0c7dd5ea232
Sagnik Mukhopadhaya <sagnikcodes.in@gmail.com>
2026-08-09 23:35:18 +0530

    Added problem 1873A

---
+++ short.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        if(s=="bca" || s=="cab") cout<<"NO"<<endl;
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+    }
+    return 0;
+}
