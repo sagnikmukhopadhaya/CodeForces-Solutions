726f472adcd7a88137ef12ca68a0afec1e16f205
Sagnik Mukhopadhaya <sagnikcodes.in@gmail.com>
2026-08-25 23:29:15 +0530

    Added problem 1606A

---
+++ ab.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        int ab=0, ba=0;
+        for(int i=0; i<s.length()-1; i++)
+        {
+            if(s[i]=='a' && s[i+1]=='b') ab++;
+            if(s[i]=='b' && s[i+1]=='a') ba++;            
+        }
+        if(ab==ba)
+            cout<<s<<endl;
+        else
+            for(int i=0; i<s.length(); i++)
+            {
+                if(s[i]=='a')
+                    s[i]='b';
+            }
+            cout<<s<<endl;
+    }
+    return 0;
+}
